b71f83c1027ea89154ae6c01613a568801c2c075
lastosellie <lastosellie@gmail.com>
2020-06-08 10:38:49 +0900

    [추가] 로그인 화면 추가

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,6 +1,7 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
@@ -10,11 +11,24 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
+//			BorderPane root = new BorderPane();
+//			Scene scene = new Scene(root,400,400);
+//			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+//			primaryStage.setScene(scene);
+//			primaryStage.show();
+			
+			// fxml 파일에서 상위 레이아웃을 가져온다.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("Login.fxml"));
+            BorderPane rootLayout = (BorderPane) loader.load();
+
+            // 상위 레이아웃을 포함하는 scene을 보여준다.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+            
+            System.out.println("java version: "+System.getProperty("java.version"));
+            System.out.println("javafx.version: " + System.getProperty("javafx.version"));
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
